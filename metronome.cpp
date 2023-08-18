100b460c35884a52f8cf7e326f39fa935aa9803c
Evan Murray <evanmurray22@gmail.com>
2023-08-18 16:26:23 -0400

    Add comments

--- metronome.cpp
+++ metronome.cpp
@@ -3,22 +3,33 @@
 #include <set>
 #include <tonc.h>
 
+// Initialize the metronome
 void init_metro(int bpm)
 {
     int seconds_per_minute = 60;
+    // Based on common denominator of CPU clock speed
     int ticks_per_second = 16384;
+    // Convert musical beat values to CPU tick values. (bpm actually stands for bars for minute here)
+    // Negative values cause timer to underflow
+    // The value of a bar in CPU ticks, negative because this causes timer to underflow at max value - beat
     int beat = -1 * seconds_per_minute * ticks_per_second / bpm;
 
+    // Set timer settings to create a 6/8 time signature (2 timers used with different subdivisions)
+    // Subdivision for melody (8th notes)
     REG_TM0D= beat / 6;
     REG_TM0CNT= TM_FREQ_1024 | TM_ENABLE;
+    // Cascade causes timer 1 to increment after timer 0 overflows (i.e. after 1/6 of a bar has passed)
     REG_TM1CNT= TM_ENABLE | TM_CASCADE;
+    // Subdivision for percussion (16th notes or 1/12 of a bar)
     REG_TM2D= beat / 12;
     REG_TM2CNT= TM_FREQ_1024 | TM_ENABLE;
+    // Cascade causes timer 3 to increment after timer 2 overflows (i.e. after 1/12 of a bar has passed)
     REG_TM3CNT= TM_ENABLE | TM_CASCADE;
 }
 
 void init_sound()
 {
+    // Setup and enable sound settings on the Gameboy with different timbres
     REG_SNDSTAT= SSTAT_ENABLE;
     REG_SNDDMGCNT= SDMG_BUILD(SDMG_SQR1 | SDMG_NOISE, SDMG_SQR1 | SDMG_SQR2, 7, 7);
     REG_SNDDSCNT= SDS_DMG100;
@@ -43,6 +54,7 @@ void set_kick_timbre(int timbre)
 
 void init_display()
 {
+    // Set the display mode to display a solid background
     REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
 
     tte_init_se_default(0, BG_CBB(0)|BG_SBB(31));
@@ -76,6 +88,8 @@ auto select_random(const S &s, size_t n) {
 
 std::vector<int> generatePrimeZero(std::string phrase)
 {
+    // Generate tone rows based on input string ascii values
+    // Fill the rest of the row with random values
     std::vector<int> p_zero;
     std::set<int> pitch_set = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
     for (int i=0; i<12; i++)
@@ -108,17 +122,25 @@ void print_status(bool isInverted, bool isRetrograde, int row_index)
 }
 
 int main() {
+    // First timer value (note melody)
     int sec = -1;
+    // Second timer value (percussion)
     int secc = -1;
+    // Note subdivision index
     int index = 0;
+    // Indices and settings for current tone row being played
     int row_index = 0;
     bool isInverted = false;
     bool isRetrograde = false;
+    // Percussion subdivision index
     int indexx = 0;
-    int notes[12] = {0, 3, 7, 11, 0, 11, 7, 3, 0, 3, 7, 11};
+    // note array for the snare
     int beat[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0};
+    // note array for the kick (2 hits)
     int kick[12] = {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+    // Second kick hit is a lower pitch (different timbre)
     int kick_timbre[12] = {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0};
+    // Array for
     int octaves[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
     init_display();
@@ -130,6 +152,8 @@ int main() {
     int tone_row[p_zero.size()][p_zero.size()];
     int i = 0;
     int adding[p_zero.size()];
+    // Calculate the rest of the tone row matrix
+    // Similar to: https://www.musictheory.net/calculators/matrix
     while(i < p_zero.size())
     {
         first_row[i] = p_zero.at(i);
@@ -189,6 +213,7 @@ int main() {
             isRetrograde = false;
             print_status(isInverted, isRetrograde, row_index);
         }
+        // When overflows happen (i.e. when the timer value changes), play each note accordingly
         if(REG_TM1D != sec)
         {
             sec = REG_TM1D;
